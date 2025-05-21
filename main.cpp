de25a61072074eb3723311fa8c3c77d4c07b5e99
danceinthestars <haruzz1337@gmail.com>
2025-05-21 17:11:51 +0800

    chore: necessary info added

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-//#test
+/**********|**********|**********|
+Program: main.cpp
+Course: OOPDS
+Trimester: 2510
+Name: Lee Ming Fung
+ID: 243UC247K7
+Lecture Section: TC3L
+Tutorial Section: TT9L
+Email: LEE.MING.FUNG@student.mmu.edu.my
+Phone: 018-2021399
+**********|**********|**********/
+
+#include <iostream>
